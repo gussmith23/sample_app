0394a7d61676427ff651a81e41d0857df4d1bb99
Gus Smith <hfs5022@psu.edu>
2014-01-03 23:52:03 -0500

    Finish static pages

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+	describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :visible => false, :text => "Ruby on Rails Sample App | Home")
+    end
+  end
+
+  describe "help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',  :visible => false, :text => "Ruby on Rails Sample App | Help")
+    end
+  end
+
+  describe "about page" do
+    
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About') 
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',  :visible => false, :text => "Ruby on Rails Sample App | About")
+    end
+  
+  end
+
+end
+
+
